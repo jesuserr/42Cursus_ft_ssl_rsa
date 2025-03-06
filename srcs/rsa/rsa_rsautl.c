a16d999eed67986e75ac6ca9840a0e7abb689365
Jesus <ivoice666@hotmail.com>
2025-03-06 15:38:02 +0100

    Bonus - Implemented Pollard's rho algorithm for public key cracking (brute force kept as backup)

--- srcs/rsa/rsa_rsautl.c
+++ srcs/rsa/rsa_rsautl.c
@@ -6,7 +6,7 @@
 /*   By: jesuserr <jesuserr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 12:03:38 by jesuserr          #+#    #+#             */
-/*   Updated: 2025/03/06 10:19:11 by jesuserr         ###   ########.fr       */
+/*   Updated: 2025/03/06 15:35:10 by jesuserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,12 +48,44 @@ static void	hexdump(t_rsa_args *args, uint64_t output)
 	close(stdout_backup);
 }
 
+// https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm
+// Since the algorithm is probabilistic, it may fail to find a factor. In that
+// case 'false' will be returned and the brute force algorithm executed.
+static bool	pollard_rho_algorithm(t_rsa_args *args)
+{
+	uint64_t	x;
+	uint64_t	y;
+
+	ft_printf("Cracking public RSA key, using Pollard's rho algorithm,");
+	ft_printf(" please wait... ");
+	x = 2;
+	y = x;
+	args->key.p = 1;
+	while (args->key.p == 1)
+	{
+		x = modular_multiplication(x, x, args->key.n) + 1;
+		y = modular_multiplication(y, y, args->key.n) + 1;
+		y = modular_multiplication(y, y, args->key.n) + 1;
+		args->key.p = greatest_common_divisor(llabs((int64_t)(x - y)), \
+		args->key.n);
+	}
+	if (args->key.p == args->key.n)
+		return (false);
+	args->key.q = args->key.n / args->key.p;
+	ft_printf(" Done!!\nn: ");
+	print_uint64_number(args->key.n);
+	ft_printf("\np: %u\nq: %u\nDecrypted message: ", args->key.p, args->key.q);
+	args->key.phi = (uint64_t)(args->key.p - 1) * (uint64_t)(args->key.q - 1);
+	args->key.d = modular_multiplicative_inverse(args->key.e, args->key.phi);
+	return (true);
+}
+
 static void	brute_force_cracker(t_rsa_args *args)
 {
 	uint64_t	i;
 
 	i = UINT32_MAX / 2;
-	ft_printf("Cracking public RSA key, please wait... ");
+	ft_printf("Cracking public RSA key, using brute force, please wait... ");
 	while (i < args->key.n)
 	{
 		if (args->key.n % i == 0)
@@ -96,7 +128,8 @@ void	rsautl(t_rsa_args *args)
 	}
 	else if (args->decrypt && args->crack)
 	{
-		brute_force_cracker(args);
+		if (!pollard_rho_algorithm(args))
+			brute_force_cracker(args);
 		output = modular_exponentiation(input, args->key.d, args->key.n);
 	}
 	modify_endianness_64_bits(&output);
